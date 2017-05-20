478ca8ef538e262eea9d04a288358b5f4b6cd047
vladiko <koganvladimir@yahoo.com>
2017-05-21 02:18:56 +0300

    adding communication constants file

--- src/app/services/communication/loginCommunicationHelper/loginCommunicationHelper.ts
+++ src/app/services/communication/loginCommunicationHelper/loginCommunicationHelper.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { CurrentUser } from '../../currentUser/currentUser';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { CommunicationConstants } from '../communicationConstants';
 // export type InternalStateType = {
 
 // };
 
 @Injectable()
 export class LoginCommunicationHelper {
-    private url = 'http://qrnewapi.azurewebsites.net/user/login';
     constructor(public currentUser: CurrentUser, private http: Http) { }
 
     public login(userName: string, userPassword: string) {
-        return this.http.post(this.url, { username: userName, password: userPassword }).map((res: Response) => {
+        return this.http.post(
+            CommunicationConstants.LOGIN_URL,
+            { username: userName, password: userPassword }
+        ).map((res: Response) => {
             let data = res.json();
             if (data.gotToken) {
                 this.currentUser.token = data.token;
@@ -22,11 +25,5 @@ export class LoginCommunicationHelper {
                 return null;
             }
         });
-        // .subscribe((data) => {
-        //     if (data.gotToken) {
-        //         this.currentUser.token = data.token;
-        //         this.currentUser.userName = userName;
-        //     }
-        // });
     }
 }
